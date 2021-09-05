0689f97c93d5825da64395e8acbc4e372d1eab1d
WilliamZhu <allwefantasy@gmail.com>
2021-09-05 10:03:00 +0800

    prepare for 0.0.4

---
+++ src/common/curve.py
@@ -0,0 +1,26 @@
+from pyjava.api.mlsql import RayContext,PythonContext
+import pandas as pd
+from pyecharts import options as opts
+from pyecharts.charts import Line
+import os
+
+context:PythonContext = context
+
+ray_context = RayContext.connect(globals(),None)
+df = ray_context.to_pandas()
+df = df.sort_values(context.conf["x"])
+
+c3=(
+   Line()
+.add_xaxis(list(df[context.conf["x"]]))
+.add_yaxis(context.conf["yName"], list(df[context.conf["y"]]))    
+.set_global_opts(title_opts=opts.TitleOpts(title=context.conf["title"]))
+.render("line_dnu.html")
+)
+
+html = ""
+with open("line_dnu.html") as file:
+   html = "\n".join(file.readlines())
+
+os.remove("line_dnu.html")
+context.build_result([{"content":html,"mime":"html"}])
